64ceaa27c9eaa38d4ba078c71d74951396290821
s3xmaxman <badside3919@gmail.com>
2024-11-14 03:19:54 +0900

    build(deps): update page.tsx, SearchForm.tsx, package-lock.json and package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
 import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -14,7 +20,7 @@ export default function Home() {
           Competitions.
         </p>
 
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
     </>
   );
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
 import { Search } from "lucide-react";
+import { Button } from "./ui/button";
 
 const SearchForm = ({ query }: { query?: string }) => {
   return (
     <Form action={"/"} scroll={false} className="search-form">
       <input
-        type="query"
+        name="query"
         defaultValue={query}
         className="search-input"
         placeholder="Search Startups"
       />
       <div className="flex gap-2">
         {query && <SearchFormReset />}
-        <button type="submit" className="search-btn text-white">
+        <Button type="submit" className="search-btn text-white">
           <Search className="size-5" />
-        </button>
+        </Button>
       </div>
     </Form>
   );
