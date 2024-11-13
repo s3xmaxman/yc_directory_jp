4c81dabe8b71ecfaf5574e16a021a959a112fb9c
s3xmaxman <badside3919@gmail.com>
2024-11-14 02:22:50 +0900

    update 5 files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div className="text-3xl">Welcome to Next js</div>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
+    </>
+  );
 }
