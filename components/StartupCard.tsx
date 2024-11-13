a209e22cfbe3b23cb97b9fa40b5ee4e4b2aac734
s3xmaxman <badside3919@gmail.com>
2024-11-14 06:48:12 +0900

    update StartupCard.tsx and next.config.ts

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,7 +1,9 @@
 import { formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
@@ -30,9 +32,31 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
             <p className="text-16-medium line-clamp-1">{name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
-            <h3 className="text-16-medium line-clamp-1">{title}</h3>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src={"https://placehold.co/600x400"}
+            alt={title}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup_card_desc">{description}</p>
+        <img src={image} alt="placeholder" className="startup-card_img" />
+      </Link>
+
+      <div className="flex-between mt-5 gap-3">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup_card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
       </div>
     </li>
   );
